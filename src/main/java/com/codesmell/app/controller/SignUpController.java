c28b3a24bb307099d98ea6981c95f1e5cd620bfe
amitsolanki8189 <amit8189@gmail.com>
2017-07-22 11:06:25 +0200

    Added the user registration

---
+++ src/main/java/com/codesmell/app/controller/SignUpController.java
@@ -0,0 +1,31 @@
+package com.codesmell.app.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.codesmell.app.directory.UserRepository;
+import com.codesmell.app.model.User;
+
+@Controller
+public class SignUpController {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@RequestMapping("/signUp")
+	public String welcome(Model model) {
+		model.addAttribute("user", new User());
+		return "signUp";
+	}
+
+	@PostMapping("/registration")
+	public String login(@ModelAttribute User user) 
+	{
+		this.userRepository.save(user);
+		return "landingPage";
+	}
+}
