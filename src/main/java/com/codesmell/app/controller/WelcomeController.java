f7890d30abd290392d644ef70a29e8f954f3190b
luca9294 <luca9294@hotmail.it>
2017-07-23 14:57:41 +0200

    Header improved and now logged as correctly displayed

--- src/main/java/com/codesmell/app/controller/WelcomeController.java
+++ src/main/java/com/codesmell/app/controller/WelcomeController.java
@@ -16,71 +16,74 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.codesmell.app.dao.UserDao;
 import com.codesmell.app.model.User;
 
-
 @Controller
 class WelcomeController {
-	
+
 	@Autowired
 	private UserDao repository;
 
-    @RequestMapping("/")
-    public String welcome(@CookieValue(value = "email", defaultValue = "") String email, Model model, HttpServletRequest req, HttpServletResponse resp) {
-        if (req.getSession().getAttribute("email") != null ){
-        		return "landingPage";
-        }
-        else if (!email.isEmpty()){
-        		req.getSession().setAttribute("email", email);
-        		return "landingPage";
-        }
-        model.addAttribute("user", new User()); 
-    	    return "welcome";
-    }
-    
-    @RequestMapping("/newproject")
-    public String newProject(Model model) {
-        return "newproject";
-    }
-    
-    @RequestMapping("/landingPage")
-    public String landingPaget(Model model, HttpServletRequest req, HttpServletResponse resp) {
-    	  if(req.getSession().getAttribute("email") == null)
-    		  return "welcome";
-    	  else{
-    		  model.addAttribute("email", req.getSession().getAttribute("email"));
-    		  return "landingPage";
-    		  }
-    }
-    
-    @RequestMapping("/logout")
-    public String logout(Model model, HttpServletRequest req, HttpServletResponse resp) {
-    	    req.getSession().removeAttribute("email");
-    	    Cookie cookie = new Cookie("email", null); // Not necessary, but saves bandwidth.
-    	    cookie.setHttpOnly(true);
-    	    cookie.setMaxAge(0); // Don't set to -1 or it will become a session cookie!
-    	    resp.addCookie(cookie);
-    	    model.addAttribute("user", new User()); 
-        return "welcome";
-    }
-    @PostMapping("/login")    
-    public String login(@ModelAttribute User user, HttpServletRequest req, HttpServletResponse resp) {
-    	        try{
-    			String emailSt = user.getEmail1();
-    	        String pwd   = user.getPwd(); 
-    	        User usr = repository.findByEmail1(emailSt);
-    	        System.out.println(usr.getPwd());
-    	        System.out.println(usr.getEmail1());
-    	        if (pwd.equals((usr.getPwd()))){
-    	          	req.getSession().setAttribute("email", emailSt);
-    	        		resp.addCookie(new Cookie("email", emailSt));
-    	          	return "landingPage";
-    	        		}
-    	        else{
-    	        		return "welcome";
-    	        }
-    	        }
-    	        catch(Exception e)
-    	        {
-    	        	return "welcome";
-    	        }
-    	    }
+	@RequestMapping("/")
+	public String welcome(@CookieValue(value = "email", defaultValue = "") String email, Model model,
+			HttpServletRequest req, HttpServletResponse resp) {
+		if (req.getSession().getAttribute("email") != null) {
+			model.addAttribute("email", req.getSession().getAttribute("email"));
+			return "landingPage";
+		} else if (!email.isEmpty()) {
+			req.getSession().setAttribute("email", email);
+			model.addAttribute("email",email);
+			return "landingPage";
+		}
+		model.addAttribute("user", new User());
+		return "welcome";
+	}
+
+	@RequestMapping("/newproject")
+	public String newProject(Model model,HttpServletRequest req, HttpServletResponse resp) {
+		model.addAttribute("email", req.getSession().getAttribute("email"));
+		return "newproject";
+	}
+
+	@RequestMapping("/landingPage")
+	public String landingPaget(Model model, HttpServletRequest req, HttpServletResponse resp) {
+		if (req.getSession().getAttribute("email") == null)
+			return "welcome";
+		else {
+			model.addAttribute("email", req.getSession().getAttribute("email"));
+			return "landingPage";
+		}
+	}
+
+	@RequestMapping("/logout")
+	public String logout(Model model, HttpServletRequest req, HttpServletResponse resp) {
+		req.getSession().removeAttribute("email");
+		Cookie cookie = new Cookie("email", null); // Not necessary, but saves
+													// bandwidth.
+		cookie.setHttpOnly(true);
+		cookie.setMaxAge(0); // Don't set to -1 or it will become a session
+								// cookie!
+		resp.addCookie(cookie);
+		model.addAttribute("user", new User());
+		return "welcome";
+	}
+
+	@PostMapping("/login")
+	public String login(Model model, @ModelAttribute User user, HttpServletRequest req, HttpServletResponse resp) {
+		try {
+			String emailSt = user.getEmail1();
+			String pwd = user.getPwd();
+			User usr = repository.findByEmail1(emailSt);
+			System.out.println(usr.getPwd());
+			System.out.println(usr.getEmail1());
+			if (pwd.equals((usr.getPwd()))) {
+				req.getSession().setAttribute("email", emailSt);
+				resp.addCookie(new Cookie("email", emailSt));
+				model.addAttribute("email", emailSt);
+				return "landingPage";
+			} else {
+				return "welcome";
+			}
+		} catch (Exception e) {
+			return "welcome";
+		}
+	}
 }
