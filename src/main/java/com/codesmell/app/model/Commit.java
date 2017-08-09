e2f1c18749383dd3b53e12ecd1f412f6841b3c02
astan <AlexandruCristian.Stan@stud-inf.unibz.it>
2017-08-09 21:25:00 +0200

    commits for projectDetails.html + minor changes to code into Commit.java & landingpage

--- src/main/java/com/codesmell/app/model/Commit.java
+++ src/main/java/com/codesmell/app/model/Commit.java
@@ -3,36 +3,36 @@ package com.codesmell.app.model;
 import java.util.Date;
 
 public class Commit extends BaseEntity{
-	private String ssa;
+	private String ssh;
 	private String idCommitAnalysis;
-	private Date creationDate;
+	private Date commitDate;
 	private Date analysisDate;
 	private String status;
 	private String projectName;
 	
 	public Commit() 
 	{
-		this.ssa="";
-		this.creationDate=new Date();
+		this.ssh="";
+		this.commitDate=new Date();
 		this.analysisDate=new Date();
 		this.status="";
 	}
 
 	public String getSsa() {
-		return ssa;
+		return ssh;
 	}
 
 	public void setSsa(String ssa) {
-		this.ssa = ssa;
+		this.ssh = ssa;
 	}
 
 
 	public Date getCreationDate() {
-		return creationDate;
+		return commitDate;
 	}
 
 	public void setCreationDate(Date creationDate) {
-		this.creationDate = creationDate;
+		this.commitDate = creationDate;
 	}
 
 	public Date getAnalysisDate() {
