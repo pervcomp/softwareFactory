12a5c19341932e50ead7e3356b255111e47b759a
luca9294 <luca9294@hotmail.it>
2017-08-26 09:30:15 +0200

    new IP

--- src/main/java/code/codesmell/app/controllerUtilities/ControllerUtilities.java
+++ src/main/java/code/codesmell/app/controllerUtilities/ControllerUtilities.java
@@ -66,7 +66,7 @@ public class ControllerUtilities {
 	private UserDao userDao;
 	private ScheduleDao scheduleDao;
 	private CommitErrorDao commitErrorDao;
-	private String urlWsVar = "http://54.214.227.88:8080";
+	private String urlWsVar = "http://34.211.54.69:8080";
 	
 
 	/**
