6f46e63fe18d57a9fcc21987fec3f5c3ef82d69b
luca9294 <luca9294@hotmail.it>
2017-09-05 13:20:24 +0200

    new ip

--- src/main/java/code/codesmell/app/controllerUtilities/ControllerUtilities.java
+++ src/main/java/code/codesmell/app/controllerUtilities/ControllerUtilities.java
@@ -81,7 +81,7 @@ public class ControllerUtilities {
 	private UserDao userDao;
 	private ScheduleDao scheduleDao;
 	private CommitErrorDao commitErrorDao;
-	private String urlWsVar = "http://34.211.54.69:8090";
+	private String urlWsVar = "http://35.160.24.252:8090";
 
 	/**
 	 * This class contains helpers methods that required from the controllers.
